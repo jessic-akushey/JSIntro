29151117290de2bb9481e2e34c26b25b01100dc7
jessic <akusheyj@yahoo.com>
2022-06-23 05:08:28 +0000

    basic descriptive js code

---
+++ about.js
@@ -0,0 +1,5 @@
+let name = window.prompt("What is your name");
+let height = window.prompt('Hi ' + name + ', what is your height in meters');
+let country = window.prompt('Which country are you from?');
+
+alert('Hi ' + name + ', you are ' + height + 'm tall. And you come from ' + country + '.');
